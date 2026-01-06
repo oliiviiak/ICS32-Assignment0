91c4a94f2e8ded43d1b55e23e964cdc2b64c5d7a
oliiviiak <242837704+oliiviiak@users.noreply.github.com>
2026-01-06 15:04:31 -0800

    downloaded starter code

---
+++ a0.py
@@ -0,0 +1,10 @@
+# a0.py
+
+# Starter code for assignment 0 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Olivia Kong
+# oykong@uci.edu
+# 92692989
+
